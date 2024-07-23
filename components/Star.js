6ab7e3c081fea659cfa5341b6f2075082e831275
Carlos Enrique R. Estrada <22361939+Carlos-Estrada@users.noreply.github.com>
2024-07-23 21:52:38 +0300

    new update

--- components/Star.js
+++ components/Star.js
@@ -3,9 +3,11 @@ import { Animated, Easing } from 'react-native';
 
 const Star = () => {
   const [position] = useState(new Animated.ValueXY({ x: 0, y: 0 }));
+  const [opacity] = useState(new Animated.Value(1)); // New state for opacity
 
-  const resetPosition = () => {
+  const resetPositionAndOpacity = () => {
     position.setValue({ x: 0, y: 0 });
+    opacity.setValue(1); // Reset opacity to full
   };
 
   const animationConfig = {
@@ -15,20 +17,38 @@ const Star = () => {
     easing: Easing.linear,
   };
 
-  const animateStarMovement = () => {
-    Animated.timing(position, animationConfig).start(() => {
-      resetPosition();
+  const twinkleConfig = {
+    duration: 1000,
+    toValue: 0,
+    useNativeDriver: true,
+    easing: Easing.inOut(Easing.quad), // This gives a gentle twinkling effect
+  };
+
+  const animateStarMovementAndTwinkle = () => {
+    // Start both animations in parallel
+    Animated.parallel([
+      Animated.timing(position, animationConfig),
+      Animated.loop(
+        Animated.sequence([
+          Animated.timing(opacity, twinkleConfig),
+          Animated.timing(opacity, { ...twinkleConfig, toValue: 1 }),
+        ]),
+        { iterations: 2 } // Number of twinkles during the movement
+      ),
+    ]).start(() => {
+      resetPositionAndOpacity();
     });
   };
 
   useEffect(() => {
-    animateStarMovement();
+    animateStarMovementAndTwinkle();
   }, []);
 
   return (
     <Animated.View
       style={{
         transform: position.getTranslateTransform(),
+        opacity: opacity, // Apply dynamic opacity
         width: 50,
         height: 50,
         backgroundColor: 'yellow',
